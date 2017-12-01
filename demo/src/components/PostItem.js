518167ce7dafeb91498099f679ae484d1a5731d8
nishan <nishanbende@gmail.com>
2017-12-01 16:25:06 +0530

    cleanUp

--- demo/src/components/PostItem.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export default class PostItem extends React.Component{
-    render() {
-        return (<div>From postItem</div>)
-    }
-}
--- demo/src/components/PostList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import PostItem from './PostItem';
-export default class PostList extends React.Component{
-    render() {
-        return (<PostItem />)
-    }
-}
